0f9c3b8d09844d54444b5253056d5838f2e9f570
Silvio Júnior <silvio_junior96@hotmail.com>
2021-09-25 11:37:27 -0300

    Configuração das Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ThanksForSigningUpComponent } from './thanks-for-signing-up/thanks-for-signing-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "sign-up", component: SignUpComponent },
+  { path: "thanks-for-signing-up", component: ThanksForSigningUpComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
