e6b4c75f2d9224b49c57afa777a4ea50c74ef325
Silvio Júnior <silvio_junior96@hotmail.com>
2021-09-30 14:55:36 -0300

    Reestruturação do componente de tela home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemProps } from '../app.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  topics: string[] = [
+    "One on one and group meetings",
+    "Record Meetings",
+    "Screen shares",
+    "Share control over screens",
+    "Schedule and send reminders",
+  ];
 
-  ngOnInit(): void {
-  }
+  globalReseller: ItemProps = {
+    name: "John Appleseed - Authorized Reseller ",
+    site: "https://www.eldorado.org.br/",
+    phone: "469-844-8261"
+  };
+
+  brazilResellers: ItemProps[] = [
+    {
+      name: "Ultratech Revendedora",
+      site: "https://www.eldorado.org.br/",
+      phone: "469-844-8261"
+    },
+    {
+      name: "BlueFin",
+      site: "https://www.eldorado.org.br/",
+      phone: "469-844-8261"
+    },
+    {
+      name: "Gold Comm Sistemas",
+      site: "https://www.eldorado.org.br/",
+      phone: "469-844-8261"
+    },
+    {
+      name: "Centraltek",
+      site: "https://www.eldorado.org.br/",
+      phone: "469-844-8261"
+    },
+  ];
 
 }
