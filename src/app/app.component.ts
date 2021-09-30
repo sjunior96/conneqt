bad14ce6e938e01338ff6879f5ea05625487fc02
Silvio Júnior <silvio_junior96@hotmail.com>
2021-09-30 14:48:06 -0300

    Criação de uma interface para representar os dados dos resellers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'conneqt-challenge';
 }
+
+export interface ItemProps {
+  name?: string;
+  site?: string;
+  phone?: string;
+}
+
